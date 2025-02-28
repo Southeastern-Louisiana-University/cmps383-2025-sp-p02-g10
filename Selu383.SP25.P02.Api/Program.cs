5e0d3b5e37813e2bc208895d1cc71485736b6d70
Cwalt2 <chris.waltersts@gmail.com>
2025-02-27 23:35:25 -0600

    Update Program.cs

--- Selu383.SP25.P02.Api/Program.cs
+++ Selu383.SP25.P02.Api/Program.cs
@@ -67,7 +67,23 @@ namespace Selu383.SP25.P02.Api
             app.UseAuthentication();
             app.UseAuthorization();
 
+
             app.MapControllers();
+            app.UseStaticFiles();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSpa(x =>
+                {
+                    x.UseProxyToSpaDevelopmentServer("http://localhost:5173");
+                });
+            }
+            else
+            {
+
+                app.MapFallbackToFile("/index.html");
+            }
+
 
             app.Run();
         }
